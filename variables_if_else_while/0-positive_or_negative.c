5055bc05be9390b10a429eba19bf177f8569df0f
rabea14180-max <rabea14180@gmail.com>
2025-10-14 10:49:37 +0000

    c0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,13 @@
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* main - Entry point */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-{
 int n;
 
 srand(time(0));
