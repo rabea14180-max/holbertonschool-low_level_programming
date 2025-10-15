af8aeaf33b115bd9af402da95881fa4bf3627dbc
rabea14180-max <rabea14180@gmail.com>
2025-10-15 12:38:33 +0000

    c2

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -12,3 +12,22 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -رات
+ *
+ * Return:
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
+}
