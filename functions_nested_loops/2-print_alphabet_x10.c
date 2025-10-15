64bcb6494e047c840d0829d8e2f88473afa30bed
rabea14180-max <rabea14180@gmail.com>
2025-10-15 11:47:14 +0000

    c2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 -رات
+*
+* Return:
+*/
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)  /* حلق
+_putchar(c);
+_putchar('\n');
+}
+}
+
