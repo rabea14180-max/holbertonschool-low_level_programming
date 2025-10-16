6ad39ce17106c48d5cc34a7010e3f36584b257ac
rabea14180-max <rabea14180@gmail.com>
2025-10-16 10:51:24 +0000

    c3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,8 +3,7 @@
 
 int _putchar(char c);  /* Prototype for function _putchar */
 void print_alphabet(void);  /* Prototype for function print_alphabet */
-
 void print_alphabet_x10(void);
-#endif
 int _islower(int c);
+
 #endif
