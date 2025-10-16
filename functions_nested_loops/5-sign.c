6052bb0534b169671c45a92dc7cce9b5d95c90f2
rabea14180-max <rabea14180@gmail.com>
2025-10-16 11:17:05 +0000

    c5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
 /**
-* _isalpha - checks for alphabetic character
-* @c: the character to check
+* print_sign - prints the sign of a number
+* @n: the number to check
 *
-* Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+* Return: 1 if n is greater than zero,
+*         0 if n is zero,
+*        -1 if n is less than zero
 */
-int _isalpha(int c)
+int print_sign(int n)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if (n > 0)
+{
+_putchar('+');
 return (1);
-else
+}
+else if (n == 0)
+{
+_putchar('0');
 return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
