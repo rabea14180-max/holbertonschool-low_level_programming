4f9cd7d75bd2902a88316aed7993e8085325b613
rabea14180-max <rabea14180@gmail.com>
2025-10-21 17:31:23 +0000

    c10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,14 @@
+#include <limits.h>
+
 /**
- * _atoi - converts a string to an integer
+ * _atoi - converts a string to an integer safely
  * @s: pointer to the string to convert
  *
  * Return: the integer value represented by the string
  *
  * Description: Converts the initial part of the string to an integer,
  * taking into account all '+' and '-' signs before the number.
- * If no numbers are found, returns 0.
+ * If no numbers are found, returns 0. Prevents signed integer overflow.
  */
 int _atoi(char *s)
 {
@@ -25,9 +27,19 @@ i++;
 
 while (s[i] >= '0' && s[i] <= '9')
 {
+
+if (result > (INT_MAX - (s[i] - '0')) / 10)
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
+
 result = result * 10 + (s[i] - '0');
 i++;
 }
 
 return (result * sign);
 }
+
