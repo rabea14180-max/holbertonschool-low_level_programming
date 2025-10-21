ac9bc47de12d883b5f482bfaa8130b74a0dcb944
rabea14180-max <rabea14180@gmail.com>
2025-10-21 11:48:22 +0000

    c9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,26 +12,17 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-
 printf("FizzBuzz");
-
 else if (i % 3 == 0)
-
 printf("Fizz");
-
 else if (i % 5 == 0)
-
 printf("Buzz");
-
 else
-
 printf("%d", i);
 
 if (i != 100)
-
 printf(" ");
-
-printf("\n");
 }
+printf("\n");
 return (0);
 }
