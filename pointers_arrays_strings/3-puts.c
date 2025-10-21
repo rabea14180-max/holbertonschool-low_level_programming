ab33c413e79f1ca2989ae95674d733f9949261b9
rabea14180-max <rabea14180@gmail.com>
2025-10-21 15:31:56 +0000

    c3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to be printed
+ *
+ * Description: This function prints each character of the string
+ * to standard output, followed by a newline character.
+ */
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+write(1, &str[i], 1); // print one character at a time
+i++;
+}
+write(1, "\n", 1);
+}
+
