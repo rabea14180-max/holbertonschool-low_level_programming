1f7326e767501e4476508c2c30d7d822ce4f3acd
rabea14180-max <rabea14180@gmail.com>
2025-10-21 16:51:23 +0000

    c8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,25 +16,4 @@ len++;
 return (len);
 }
 
-/**
- * puts_half - prints the second half of a string
- * @str: pointer to the string
- *
- * Description: If the string length is odd, prints the last
- * (length + 1)/2 characters. Always prints a newline at the end.
- */
-void puts_half(char *str)
-{
-int len = _strlen(str);
-int start, i;
-
-if (len % 2 == 0)
-start = len / 2;
-else
-start = (len + 1) / 2;
-
-for (i = start; i < len; i++)
-write(1, &str[i], 1);
-
-write(1, "\n", 1);
 }
