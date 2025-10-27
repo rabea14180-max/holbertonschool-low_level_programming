5ba5024cb5990c92b32455773c57e097846efab2
rabea14180-max <rabea14180@gmail.com>
2025-10-27 08:05:06 +0000

    c3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search in
+ * @accept: string containing the characters to match
+ *
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+char *a;
+
+while (*s)
+{
+a = accept;
+while (*a)
+{
+if (*s == *a)
+break;
+a++;
+}
+if (*a == '\0')
+return count;
+count++;
+s++;
+}
+
+return count;
+}
