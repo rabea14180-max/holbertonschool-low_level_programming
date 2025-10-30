212982d295297250cf6db8fc440d56a6249789fb
rabea14180-max <rabea14180@gmail.com>
2025-10-30 10:13:00 +0000

    c3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers
- * @argc: number of command-line arguments
- * @argv: array of command-line arguments
- *
- * Return: 0 if successful, 1 if error
+	 * main - multiplies two numbers
+	 * @argc: number of command-line arguments
+	 * @argv: array of command-line arguments
+	 *
+	 * Return: 0 if successful, 1 if error
  */
-int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 {
-int num1, num2, result;
+	int num1, num2, result;
 
 if (argc != 3)
 {
@@ -20,7 +20,7 @@ return (1);
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 
 printf("%d\n", result);
 return (0);
