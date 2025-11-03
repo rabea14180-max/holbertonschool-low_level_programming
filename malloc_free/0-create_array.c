4b318cff00ba970ec0c53cdf9209cd8c17e25ca1
rabea14180-max <rabea14180@gmail.com>
2025-11-03 12:38:28 +0000

    c0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and initializes it
+ * with a specific char
  * @size: size of the array
  * @c: char to initialize the array with
  *
