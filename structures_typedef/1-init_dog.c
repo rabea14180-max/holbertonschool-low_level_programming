1756a2e432fe26440bf10ad207d4231bc412d200
rabea14180-max <rabea14180@gmail.com>
2025-11-08 13:54:37 +0000

    c1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to assign
+ * @age: age to assign
+ * @owner: owner to assign
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,6 +13,9 @@ char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
 
 #endif
 
