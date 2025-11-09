af45da6aa39e90399bad14e089e3ceda4e98f052
rabea14180-max <rabea14180@gmail.com>
2025-11-09 11:51:42 +0000

    c2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,28 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+/**
+ * struct dog - structure for dog info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 #endif
