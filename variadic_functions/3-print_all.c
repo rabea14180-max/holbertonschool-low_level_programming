6546238c4a76bc32dde32a8fd27e28c9fb56c666
rabea14180-max <rabea14180@gmail.com>
2025-11-12 11:15:54 +0000

    c3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,13 +4,6 @@
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
- *
- * Description:
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil))
- * any other char should be ignored
  */
 void print_all(const char * const format, ...)
 {
@@ -21,25 +14,28 @@ char *str, *sep = "";
 va_start(args, format);
 while (format && format[i])
 {
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
+switch (format[i])
 {
-printf("%s", sep);
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-else if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-else if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-else if (format[i] == 's')
-{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
-if (str == NULL)
+if (!str)
 str = "(nil)";
-printf("%s", str);
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
 }
 sep = ", ";
-}
 i++;
 }
 printf("\n");
