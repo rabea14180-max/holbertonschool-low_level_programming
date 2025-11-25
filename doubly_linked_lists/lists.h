574d35f6501e1cfd455ae16abcd0b16db98e0c81
rabea14180-max <rabea14180@gmail.com>
2025-11-25 11:05:39 +0000

    c0

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS.H
+#define LISTS.H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer stored in the node
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton/ALX project
+ */
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+
+
+#endif
