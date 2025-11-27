2f4772473c53c8e1f51aa6ae127ba306ea1ac1bc
rabea14180-max <rabea1418@gmail.com>
2025-11-27 13:57:08 +0000

    c1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes text_content to it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, w;
+size_t len = 0;
+
+if (filename == NULL)
+return (-1);
+
+if (text_content != NULL)
+{
+while (text_content[len] != '\0')
+len++;
+}
+
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+
+
+if (len > 0)
+{
+w = write(fd, text_content, len);
+if (w == -1 || (size_t)w != len)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
+}
