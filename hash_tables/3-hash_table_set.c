3fd112c2ebb3e9a85e69014d917e3fa42d2ac0ba
Your Name <you@example.com>
2025-12-08 15:24:22 +0000

    c3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 #include <string.h>
-
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to hash table
@@ -14,7 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int idx;
 hash_node_t *node, *tmp;
 char *value_dup;
-
 if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 return (0);
 idx = key_index((const unsigned char *)key, ht->size);
