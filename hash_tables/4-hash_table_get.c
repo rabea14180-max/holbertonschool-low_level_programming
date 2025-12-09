33f50e72c416f0a10506aeed13d360166bdb0140
rabea14180-max <rabea14180@gmail.com>
2025-12-09 11:03:36 +0000

    c4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: key you are looking for
+ *
+ * Return: value associated with key, or NULL if key is not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+hash_node_t *node;
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+while (node != NULL)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
+}
+return (NULL);
+}
